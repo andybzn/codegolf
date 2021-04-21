28cbc060e8fb599c20e1955ee68dba1f2bc182e1
Andy <9117837+dark-coffee@users.noreply.github.com>
2021-04-21 22:42:31 +0100

    Create covfefify.ps1

---
+++ covfefify.ps1
@@ -0,0 +1,4 @@
+#covefify
+$w = "coverage"
+#324 bytes
+$v="[aeiouy]";$f=[regex]::match($w,$v).index+1;$s=[regex]::match($w.substring($f),"[^aeiouy]").index;$t=[regex]::match($w.substring($f),$v);$c=@{b='p';c='g';d='t';f='v';g='k';h='h';j='j';k='g';l='l';m='m';n='n';p='b';q='q';r='r';s='z';t='d';v='f';w='w';x='x';z='s'};$r=$c["$($w[($f+$s)])"];$w.substring(0,$f+$s+1)+"$r$t$r$t"
